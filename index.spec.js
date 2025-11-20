bf4675324cffc67eed64b98fdcc0c2980282c746
Wesley92Silva <wesley92silva@gmail.com>
2025-11-20 20:42:04 -0300

    First failling test

---
+++ index.spec.js
@@ -0,0 +1,8 @@
+const calculaComissaoVenda = require('.')
+
+it('Se a lista estiver vazia, a comissão é zero', () =>{
+    const resultadoAtual = calculaComissaoDeVenda([])
+    const resultadoEsperado = 0
+
+    expect(resultadoAtual).toBe(resultadoEsperado)
+})
