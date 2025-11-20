08b521ff76031e4dedf74c5be8f459cc9ef7b543
Wesley92Silva <wesley92silva@gmail.com>
2025-11-20 20:56:01 -0300

    Add another failing test

--- index.spec.js
+++ index.spec.js
@@ -6,3 +6,14 @@ it('Se a lista estiver vazia, a comissão é zero', () =>{
 
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
+
+it('Calcula comissão quando tem apenas um item na lista', () =>{
+    const resultadoAtual = calculaComissaoDeVenda([{
+        id: 'PROD-123',
+        precoUnitario: 1000,
+        quantidadeVendida: 1
+    }])
+    const resultadoEsperado = 50
+
+    expect(resultadoAtual).toBe(resultadoEsperado)
+})
