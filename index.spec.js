9931d7e35cea9534926cd466ca0afbe9fb1cf4fc
Wesley92Silva <wesley92silva@gmail.com>
2025-11-20 22:10:07 -0300

    Add another failling testing

--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,13 @@
 const calculaComissaoDeVenda = require('.')
 
-it('Se a lista estiver vazia, a comissão é zero', () =>{
+it('Se a lista estiver vazia, a comissão é zero', () => {
     const resultadoAtual = calculaComissaoDeVenda([])
     const resultadoEsperado = 0
 
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
 
-it('Calcula comissão quando tem apenas um item na lista', () =>{
+it('Calcula comissão quando tem apenas um item na lista', () => {
     const resultadoAtual = calculaComissaoDeVenda([{
         id: 'PROD-123',
         precoUnitario: 1000,
@@ -18,18 +18,30 @@ it('Calcula comissão quando tem apenas um item na lista', () =>{
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
 
-it('Calcula comissão quando há mais de um item na lista', () =>{
+it('Calcula comissão quando há mais de um item na lista', () => {
     const resultadoAtual = calculaComissaoDeVenda([{
         id: 'PROD-123',
         precoUnitario: 1000,
         quantidadeVendida: 1,
-    },{
+    }, {
         id: 'PROD-465',
         precoUnitario: 100,
         quantidadeVendida: 5,
     }])
 
     const resultadoEsperado = 75
+
+    expect(resultadoAtual).toBe(resultadoEsperado)
+})
+
+it('Calcula a comissão de 10%', () => {
+    const resultadoAtual = calculaComissaoDeVenda([{
+        id: 'PROD-789',
+        precoUnitario: 2000,
+        quantidadeVendida: 1,
+    }])
+
+    const resultadoEsperado = 200
 
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
